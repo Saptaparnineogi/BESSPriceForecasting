2ae545c36c890e34ddab898756db2afa0ea8d7cd
Saptaparni Neogi <neogi.saptaparni@gmail.com>
2026-06-24 16:55:31 +0200

    Correcting print in evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -10,10 +10,10 @@ def evaluate_lag_baseline(train, test, features):
     )
     print(f"Naive lag-336 MAE: {lag_mae:.3f}")
 
-    
+
 def evaluate_model(model, X_test, y_test):
     y_pred = model.predict(X_test)
     mae = mean_absolute_error(y_test, y_pred)
     rmse = np.sqrt(mean_squared_error(y_test, y_pred))
-    print(f"XGB MAE: {mae:.3f}")
-    print(f"XGB RMSE: {rmse:.3f}")
+    print(f"Model MAE: {mae:.3f}")
+    print(f"Model RMSE: {rmse:.3f}")
